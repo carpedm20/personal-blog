8e289a31eafa02355ad56097ba0e881bc6fd8e2a
carpedm20 <carpedm20@gmail.com>
2013-09-17 12:40:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Personal blog, designed at 2013.09.17
+
+Copyright (c) 2013 Taehoon Kim
+
+contact : carpedm20@gmail.com
---
+++ app/views/post.html.php
@@ -0,0 +1,25 @@
+<div class="post">
+
+	<h2><?php echo $p->title ?></h2>
+
+	<div class="date"><?php echo date('d F Y', $p->date)?></div>
+
+	<?php echo $p->body?>
+
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'carpedm20'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+
+</div>
+
