e7b096e61ce73ddf70a76db8b84282f5eed22fe7
carpedm20 <carpedm20@gmail.com>
2013-09-18 11:07:50 +0900

    tools update

--- app/views/etc.html.php
+++ app/views/etc.html.php
@@ -1,46 +1,60 @@
-<div class="post">
-
-   <script src="<?echo config('site.url')?>assets/js/carpedm20_etc.js"></script>
+<div class="post">	
+ <div class="etc">
    <h2>ETC</h2>
-   <br>
-   <h3>Google Search</h3>
-   <div id="google">
+  <div id="gallery">
+   <div id="slides">
+   <script src="<?echo config('site.url')?>assets/js/carpedm20_etc.js"></script>
+
+   <div class="slide" id="quote">
+    <h3>Quotes</h3>
+    <div id="container">
+     Carpe Diem
+    </div>
    </div>
-   <br>
-   <h3>Quotes - 2013.09</h3>
-   <div id="container">오늘을 즐겨라 - carpedm20</div>
-   <br>
+
+   <div class="slide" id="hexabot">
    <h3>HeXA Bot - 2013.06</h3>
-   <div id="hexabot">
-   <br>
    <a href="https://pypi.python.org/pypi/kakao"><img src="<?echo config('site.url')?>uploads/hexabot2.png" style="width:100%"></a>
    </div>
-   <br>
+
+   <div class="slide" id="portalbot">
    <h3>Portal Bot - 2013.05</h3>
-   <div id="portalbot">
-   <br>
    <a href="https://www.facebook.com/hexa.portal" style="opacity:0.8;"><img src="<?echo config('site.url')?>uploads/portalbot1.png" style="width:100%"></a>
    </div>
-   <br>
+
+   <div class="slide" id="foodbot">
    <h3>Food Bot - 2013.05</h3>
-   <div id="foodbot">
-   <br>
    <a href="https://www.facebook.com/hexa.food.bot"><img src="<?echo config('site.url')?>uploads/foodfacebook.png" style="width:100%"></a>
    </div>
-   <br>
+
+   <div class="slide" id="robot"> 
    <h3>Robot - 2013.03</h3>
-   <div id="robot" style="text-align: center;">
-   <br>
-   <a href="https://www.facebook.com/hexa.food.bot"><img src="<?echo config('site.url')?>uploads/robot.png" style="width:60%"></a>
-   <br>
-   <br>
-   <a href="https://www.facebook.com/hexa.food.bot"><img src="<?echo config('site.url')?>uploads/screen2.png" style="width:100%"></a>
+    <div style="text-align:center">
+     <a href="https://github.com/carpedm20/UNIST-robot"><img src="<?echo config('site.url')?>uploads/robot.png" style="width:60%"></a>
+     <a href="https://github.com/carpedm20/UNIST-robot"><img src="<?echo config('site.url')?>uploads/screen2.png" style="width:80%"></a>
+    </div>
    </div>
-   <br>
+
+   <div class="slide" id="mozo">
    <h3>Mozo Killer - 2012.10</h3>
-   <div id="mozo" style="text-align: center;">
-   <br>
-   <a href="https://www.facebook.com/hexa.food.bot"><img src="<?echo config('site.url')?>uploads/mozo.png" style="width:70%"></a>
+    <div style="text-align:center">
+     <a href="https://github.com/carpedm20/KIRD-killer"><img src="<?echo config('site.url')?>uploads/mozo.png" style="width:70%"></a>
+    </div>
    </div>
-   <br>
+   </div>
+   <!--
+   <div id="menu">
+   <ul>
+    <li class="fbar">&nbsp;</li>
+    <li class="menuItem">
+     <a href=""><img src="<?echo config('site.url')?>uploads/drawing.png" /></a></li>
+    <li class="menuItem"><a href=""><img src="<?echo config('site.url')?>uploads/drawing.png"></a></li>
+    <li class="menuItem"><a href=""><img src="<?echo config('site.url')?>uploads/drawing.png"></a></li>
+    <li class="menuItem"><a href=""><img src="<?echo config('site.url')?>uploads/drawing.png"></a></li>
+    <li class="menuItem"><a href=""><img src="<?echo config('site.url')?>uploads/drawing.png"></a></li>
+    <li class="menuItem"><a href=""><img src="<?echo config('site.url')?>uploads/drawing.png"></a></li>
+    </ul>
+    </div> -->
+  </div>
+ </div>
 </div>
--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -24,7 +24,7 @@
 	<![endif]-->
 	
 </head>
-<body>
+<body bgcolor="white">
 
 	<aside>
 
